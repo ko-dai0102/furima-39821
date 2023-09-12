a0da61bd95a581845eeb4bc124aa8ee67483b433
ko-dai0102 <koudai.tanno@gmail.com>
2023-09-12 14:43:59 +0900

    emailバリデーションの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, uniqueness: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
